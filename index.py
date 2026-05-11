f53d6fdece01cd4c15518aac86d638fd701715b3
Vinicius Granco <viniciusgrancogoncalves@gmail.com>
2026-05-11 17:57:43 -0300

    usando boxplot

--- index.py
+++ index.py
@@ -43,6 +43,62 @@ Tabela_sinais.rename(columns={'PARÂMETRO': 'PARAMETRO', 'VALOR': 'VALOR NUMÉRI
 Tabela_hemogramas.dropna(how="all", inplace=True)
 Tabela_sinais.dropna(how="all", inplace=True)
 
+Tabela_hemogramas["EXAME"] = Tabela_hemogramas["EXAME"].str.strip()
+Tabela_hemogramas["PARAMETRO"] = Tabela_hemogramas["PARAMETRO"].str.strip()
+
+Tabela_sinais["EXAME"] = Tabela_sinais["EXAME"].str.strip()
+Tabela_sinais["PARAMETRO"] = Tabela_sinais["PARAMETRO"].str.strip()
+
+#%% arrumar itens
+substituicoes_exame = {
+    "LP-SÓDIO - NA": "LP-SÓDIO-NA",
+    "LP-POTÁSSIO - K": "LP-POTÁSSIO-K",
+    "LP-PÓTASSIO-K": "LP-POTÁSSIO-K",
+    "LP-POTASSIO-K": "LP-POTÁSSIO-K",
+    "LP-CLORO - CLORETO": "LP-CLORO-CLORETO",
+    "LP-ÁCIDO LÁCTICO - LACTATO": "LP-ÁCIDO LÁCTICO-LACTATO",
+    "LP-CULTURA DE BACTERIA-ASPIRADO TRAQUEAL 1 AMOSTRA": 'LP-CULTURA DE BACTÉRIA - ASPIRADO TRAQUEAL 1ª AMOSTRA',
+    "LP-ALANINA AMINOTRANSFERASE - (ALT/TGP)": "LP-ALANINA AMINOTRANSFERASE-(ALT/TGP)",
+    "LP-CREATINOFOSFOQUINASE - CPK - CK": "LP-CREATINOFOSFOQUINASE-CPK-CK",
+    "LP-TAP - TEMPO DE ATIVIDADE DE PROTROMBINA": "LP-TAP -TEMPO DE ATIVIDADE DE PROTROMBINA",
+    "LP-TESTE RÁPIDO - SÍFILIS - TREPONEMA PALLIDUM": "LP-TESTE RÁPIDO - SÍFILIS-TREPONEMA PALLIDUM",
+    "LP-ASPARTATO AMINOTRANSFERASE - (AST/TGO) - (AST/TGO)": "LP-ASPARTATO AMINOTRANSFERASE - (AST/TGO)-(AST/TGO)",
+    "LP-TTPA - TEMPO DE TROMBOPLASTINA PARCIAL ATIVADO": "LP-TTPA-TEMPO DE TROMBOPLASTINA PARCIAL ATIVADO"
+}
+
+substituicoes_parametro = {
+    "LP-CLORO - CLORETO": "LP-CLORO-CLORETO",
+    "Microorganismo Isolado": "Microorganismos Isolados",
+    "Microorganismo Isolado:": "Microorganismos Isolados",
+    "Microorganismo isolado": "Microorganismos Isolados",
+    "Microorganismo isolado:": "Microorganismos Isolados",
+    "Microorganismos Isolados:": "Microorganismos Isolados",
+    "Proteinas Totais": "Proteínas Totais",
+    "Proteinas Totais  ": "Proteínas Totais",
+    "Tempo de positividade:": "Tempo de positividade",
+    "Base excess(BE)": "Base Excess(BE)",
+    "CO2 Total": "CO2 TOTAL",
+    "Relacao A/G": "Relação A/G",
+    "Τ": "T",
+    "Ρ": "P",
+    "Α": "A",
+    "Ο": "O"
+}
+
+for antigo, novo in substituicoes_exame.items():
+    Tabela_hemogramas["EXAME"] = (
+        Tabela_hemogramas["EXAME"]
+        .str.replace(antigo, novo, regex=False)
+    )
+
+for antigo, novo in substituicoes_parametro.items():
+    Tabela_hemogramas["PARAMETRO"] = (
+        Tabela_hemogramas["PARAMETRO"]
+        .str.replace(antigo, novo, regex=False)
+    )
+    
+for valor in sorted(Tabela_hemogramas["EXAME"].dropna().unique()):
+    print(repr(valor))
 #%% Processamento Hemogramas
 
 # for col in Tabela_hemogramas.columns:
@@ -141,7 +197,7 @@ def tab_paciente_numerico(Tabela_hemogramas, Tabela_sinais, paciente_nome):
     return tabela
 
 def tab_exame_numerico(Tabela_hemogramas, Tabela_sinais, exame_nome):
-    Tabela_hemogramas_filtrado = Tabela_hemogramas[Tabela_hemogramas['EXAME'] + 
+    Tabela_hemogramas_filtrado = Tabela_hemogramas[Tabela_hemogramas['EXAME'] + " " +
                                                    Tabela_hemogramas['PARAMETRO']== exame_nome]
     
     tabela1 = Tabela_hemogramas_filtrado.pivot_table(
@@ -163,10 +219,13 @@ def tab_exame_numerico(Tabela_hemogramas, Tabela_sinais, exame_nome):
     tabela = pd.concat([tabela1, tabela2])
     return tabela
 
-A = tab_paciente_numerico(Tabela_hemogramas, Tabela_sinais, "RN004")
+A = tab_exame_numerico(Tabela_hemogramas, Tabela_sinais, "LP-HEMOGRAMA COMPLETOBasófilos")
 
 import re
-def gerar_graficos(df, pasta_saida="GRAFICOS"):
+import numpy as np
+from matplotlib.lines import Line2D
+
+def gerar_graficos(df, pasta_saida="ProjetoAlessandra/GRAFICOS"):
 
     df_plot = df.copy()
 
@@ -207,39 +266,61 @@ def gerar_graficos(df, pasta_saida="GRAFICOS"):
 
         agrupado = agrupado.sort_values('DIARELATIVO')
 
-        x_pos = range(len(agrupado))
-
         plt.figure(figsize=(18, 9))
 
-        plt.errorbar(
-            x_pos,
-            agrupado['media'],
-            yerr=agrupado['std'],
-            fmt='-o',
-            capsize=5
+        dias = sorted(por_paciente['DIARELATIVO'].unique())
+        
+        dados_plot = [
+            por_paciente.loc[
+                por_paciente['DIARELATIVO'] == dia,
+                'VALOR NUMÉRICO'
+            ].dropna().values
+            for dia in dias
+        ]
+        
+        # 🔹 boxplot
+        plt.boxplot(
+            dados_plot,
+            tick_labels=dias,
+            showfliers=True,
+            showmeans=True,
+            meanline=True,
+            medianprops=dict(color='red'),
+            meanprops=dict(color='blue')
         )
-
-        plt.xticks(
-            x_pos,
-            agrupado['DIARELATIVO'].astype(int),
-            rotation=0
-        )
-
+        
+        plt.legend(handles=[
+            Line2D([0], [0], color='red', lw=2, label='Mediana'),
+            Line2D([0], [0], color='blue', lw=2,  linestyle='--', label='Média')
+        ])
+        
         # 🔹 anotações
-        for i, row in agrupado.iterrows():
-            if pd.notna(row['media']):
-                plt.text(
-                    i,
-                    row['media'] * 1.01,
-                    f"{row['media']:.2f}±{row['std']:.2f}\n(n={int(row['n'])})",
-                    ha='center',
-                    va='bottom',
-                    fontsize=8
-                )
-
+        for i, valores in enumerate(dados_plot):
+        
+            media = np.mean(valores)
+            std = np.std(valores)
+            n = grupo.loc[
+                grupo['DIARELATIVO'] == dias[i],
+                'PACIENTE'
+            ].nunique()
+            
+            if len(valores) == 0:
+                continue
+            ymax = np.max(valores)
+        
+            plt.text(
+                i + 1,
+                ymax * 1.02,
+                f"{media:.2f}±{std:.2f}\n(n={n})",
+                ha='center',
+                va='bottom',
+                fontsize=8
+            )
+        
         plt.xlabel('Dia Relativo')
-        plt.ylabel('Valor Médio')
+        plt.ylabel('Valor')
         plt.title(nome)
+        
         plt.grid()
         plt.tight_layout()
 
